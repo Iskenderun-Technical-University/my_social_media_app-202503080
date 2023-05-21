9033d3117ea493bbe115df9a77c2cdc0e54d9639
Sevda Abbasi <sevdaabbasi37@gmail.com>
2023-05-22 02:23:58 +0300

    design creation

--- mySocialMediaApp/ViewController.swift
+++ mySocialMediaApp/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var usernameText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toTabBar", sender: nil)
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
+    
+    
+    
+   
 }
 
