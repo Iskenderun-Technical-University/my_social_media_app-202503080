7cb76e422c8c97eb8c6126ccc9f1967e504ff468
Sevda Abbasi <sevdaabbasi37@gmail.com>
2023-06-05 23:58:48 +0300

    firebase operations

--- mySocialMediaApp/addQuestionViewController.swift
+++ mySocialMediaApp/addQuestionViewController.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import FirebaseStorage
+import FirebaseAuth
+import Firebase
+
 
 class addQuestionViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -41,8 +45,58 @@ class addQuestionViewController: UIViewController, UIImagePickerControllerDelega
         
     }
     
+    func makeAlert(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+        
+    }
+    
 
     @IBAction func actionUploadButton(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        let mediaFolder = storageReference.child("media").child("images") // firebase içerisinde "media" ve "images" isimlerinde iki depolama klasörü oluşturur
+        
+        // görseli veriye çevirerek firebase öyle atmamızı sağlar
+        //"compressionQuality: 0.5 " 0.5 kadar sıkıştırma işlemini yapar.
+        if let  data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            imageReference.putData(data) { (metadata, error) in
+                if error != nil {
+                    self.self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                    
+                }else{
+                    imageReference.downloadURL { (url, error) in
+                        if error == nil {
+                            let imageUrl = url?.absoluteString // Url yi Stringe çevirir
+                           
+                            //DATABASE
+                            
+                            let firestoreDatabase = Firestore.firestore()
+                            
+                            var firestoreReference : DocumentReference? = nil
+                            
+                            let firestorePost = ["imageUrl" : imageUrl!, "postedBy" : Auth.auth().currentUser!.email!, "postCommend" : self.commentText.text!, "date" : "date", "likes" : 0] as [String : Any]
+
+                            firestoreReference = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { (error) in
+                                if error != nil {
+                                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                                }
+                            })
+                            
+                        }
+                    }
+                }
+            }
+            
+        }
+           
     }
     
 }
